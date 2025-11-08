e036912c761e1ec30ab8b5cdd27fc25dd1e3f5cf
amanpandey2101 <paman7681@gmail.com>
2025-11-08 21:24:58 +0530

    Authentication complete

---
+++ app/(dashboard)/workspace/layout.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const WorkspaceLayout = ({children}:{children:React.ReactNode}) => {
+  return (
+    <div className='flex w-full h-screen'>
+        <div className='flex h-full w-16 flex-col items-center bg-secondary py-3 px-2 border-r border-border'>
+
+        </div>
+    </div>
+  )
+}
+
+export default WorkspaceLayout
